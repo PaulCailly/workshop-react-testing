f04ac62ef8f4339fbb4ee33ee5d8d84c0d9562f8
Paul Cailly <hello@paulcailly.fr>
2021-02-14 09:27:35 +0100

    remove useless test

--- workshop/app/src/tests/4-simulating-user-interation.test.js
+++ workshop/app/src/tests/4-simulating-user-interation.test.js
@@ -3,14 +3,6 @@ import { render, fireEvent } from "@testing-library/react";
 
 import { App } from "../App";
 
-test("renders the correct content", () => {
-  const { getByText, getByLabelText } = render(<App />);
-
-  getByText("Liste de courses");
-  getByLabelText("Qu'est ce que je dois acheter ?");
-  getByText("Ajouter #1");
-});
-
 test("allows users to add items to their list", () => {
   const { getByText, getByLabelText } = render(<App />);
 
